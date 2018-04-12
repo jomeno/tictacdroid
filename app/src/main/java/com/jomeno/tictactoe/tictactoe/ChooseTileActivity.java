cfe0d68f3037f8d7ee064d098770195e3e3b2cd4
Jomeno Vona <jomeno.vona@gmail.com>
2018-04-12 15:23:24 +0100

    Added akp

--- app/src/main/java/com/jomeno/tictactoe/tictactoe/ChooseTileActivity.java
+++ app/src/main/java/com/jomeno/tictactoe/tictactoe/ChooseTileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ChooseTileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,6 +23,11 @@ public class ChooseTileActivity extends AppCompatActivity implements View.OnClic
         Intent intent = getIntent();
         boardSize = intent.getIntExtra(EXTRA_BOARD_SIZE, boardSize);
         isOpponentAi = intent.getBooleanExtra(EXTRA_OPPONENT_PLAYER, isOpponentAi);
+
+        ImageView noughtMenuItem = findViewById(R.id.choice_nought);
+        noughtMenuItem.setOnClickListener(this);
+        ImageView crossMenuItem = findViewById(R.id.choice_cross);
+        crossMenuItem.setOnClickListener(this);
     }
 
     @Override
@@ -35,12 +41,11 @@ public class ChooseTileActivity extends AppCompatActivity implements View.OnClic
             case R.id.choice_nought:
                 tileChoice = false;
                 intent.putExtra(EXTRA_TILE_CHOICE, tileChoice);
+                startActivity(intent);
                 break;
             case R.id.choice_cross:
                 tileChoice = true;
                 intent.putExtra(EXTRA_TILE_CHOICE, tileChoice);
-                break;
-            default:
                 startActivity(intent);
                 break;
         }
